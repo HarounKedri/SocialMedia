81ec92f771610da6fd44c246044bd216d1c2ef2e
Haroun <haroun@apprentus.com>
2024-06-09 21:03:29 +0100

    fixed delete POST / fixed new POST order

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -27,21 +27,23 @@ export const createPost = async (req, res) => {
       comments: [],
     });
 
-    await newPost.save();
+    const savedPost = await newPost.save();
 
-    const post = await Post.find();
-    res.status(201).json(post);
+    // Return the newly created post
+    res.status(201).json(savedPost);
   } catch (err) {
     console.error("Error creating post:", err); // Log the error for debugging
     res.status(409).json({ message: err.message });
   }
 };
 
+
+
 /* READ */
 export const getFeedPosts = async (req, res) => {
   try {
-    const post = await Post.find();
-    res.status(200).json(post);
+    const posts = await Post.find().sort({ createdAt: -1 });
+    res.status(200).json(posts);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
@@ -50,12 +52,25 @@ export const getFeedPosts = async (req, res) => {
 export const getUserPosts = async (req, res) => {
   try {
     const { userId } = req.params;
-    const post = await Post.find({ userId });
-    res.status(200).json(post);
+    const posts = await Post.find({ userId }).sort({ createdAt: -1 });
+    res.status(200).json(posts);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
 };
+
+
+/* DELETE */
+export const deletePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Post.findByIdAndDelete(id);
+    res.status(200).json({ message: 'Post deleted successfully' });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
 
 /* UPDATE */
 export const likePost = async (req, res) => {
